cffceeb7494ed804769244774b24bf15cdd0d59f
Christian Dokter <328370@student.mboutrecht.nl>
2019-11-18 13:07:16 +0100

    Initial Commit

---
+++ Home.php
@@ -0,0 +1,6 @@
+<div class="row">
+    <div class="col-6">
+    <img src="./img/Kerstboom2" alt="Kerstboom2" class="img-fluid">
+    </div>
+    <div class="col-6"><h1>Kerst avond komt er weer aan</h1></div>
+</div>
